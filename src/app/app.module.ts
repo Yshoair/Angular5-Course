e3a04d9c71c6b66f78f9f296b8fa32e7af4af148
Yusuf Alaa-Eldeen <yshoair@gmail.com>
2018-02-05 13:33:12 +0200

    Added server component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,14 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-
+import {ServerCompnent} from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerCompnent,
+    ServersComponent
   ],
   imports: [
     BrowserModule
